9e823e71d5923c9fc338101185b4e5d64882793e
Kacper Kondracki <43544435+Kacper-Kondracki@users.noreply.github.com>
2025-08-31 21:20:07 +0200

    Fix anime db download

--- ShindenToAnilist/frontend/wailsjs/go/models.ts
+++ ShindenToAnilist/frontend/wailsjs/go/models.ts
@@ -50,7 +50,7 @@ export namespace db {
 		    if (!a) {
 		        return a;
 		    }
-		    if (a.slice) {
+		    if (a.slice && a.map) {
 		        return (a as any[]).map(elem => this.convertValues(elem, classs));
 		    } else if ("object" === typeof a) {
 		        if (asMap) {
@@ -97,7 +97,7 @@ export namespace main {
 		    if (!a) {
 		        return a;
 		    }
-		    if (a.slice) {
+		    if (a.slice && a.map) {
 		        return (a as any[]).map(elem => this.convertValues(elem, classs));
 		    } else if ("object" === typeof a) {
 		        if (asMap) {
@@ -146,7 +146,7 @@ export namespace searcher {
 		    if (!a) {
 		        return a;
 		    }
-		    if (a.slice) {
+		    if (a.slice && a.map) {
 		        return (a as any[]).map(elem => this.convertValues(elem, classs));
 		    } else if ("object" === typeof a) {
 		        if (asMap) {
@@ -178,7 +178,7 @@ export namespace searcher {
 		    if (!a) {
 		        return a;
 		    }
-		    if (a.slice) {
+		    if (a.slice && a.map) {
 		        return (a as any[]).map(elem => this.convertValues(elem, classs));
 		    } else if ("object" === typeof a) {
 		        if (asMap) {
