9e93cd5e4c4a15decf442b91b4c5e7b70b472633
Kacper-Kondracki <43544435+Kacper-Kondracki@users.noreply.github.com>
2026-06-16 00:00:47 +0200

    Add protoc arg so github actions can build

--- crates/shinden-to-anilist-grpc/build.rs
+++ crates/shinden-to-anilist-grpc/build.rs
@@ -1,15 +1,17 @@
 const PROTO_PATH: &str = "../../proto";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::configure().compile_protos(
-        &[
-            format!("{PROTO_PATH}/shinden_to_anilist/v1/service.proto"),
-            format!("{PROTO_PATH}/shinden_to_anilist/v1/error.proto"),
-            format!("{PROTO_PATH}/shinden_to_anilist/v1/search.proto"),
-            format!("{PROTO_PATH}/shinden_to_anilist/v1/matching.proto"),
-            format!("{PROTO_PATH}/shinden_to_anilist/v1/export.proto"),
-        ],
-        &[PROTO_PATH.into()],
-    )?;
+    tonic_prost_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile_protos(
+            &[
+                format!("{PROTO_PATH}/shinden_to_anilist/v1/service.proto"),
+                format!("{PROTO_PATH}/shinden_to_anilist/v1/error.proto"),
+                format!("{PROTO_PATH}/shinden_to_anilist/v1/search.proto"),
+                format!("{PROTO_PATH}/shinden_to_anilist/v1/matching.proto"),
+                format!("{PROTO_PATH}/shinden_to_anilist/v1/export.proto"),
+            ],
+            &[PROTO_PATH.into()],
+        )?;
     Ok(())
 }
